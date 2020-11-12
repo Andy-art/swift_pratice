2ffe0efc47e7ff509590f049d1673a88ca2666ed
bye9 <tjrwjdghks12@naver.com>
2020-11-12 19:30:12 +0900

    변수와 상수

--- test/main.swift
+++ test/main.swift
@@ -5,7 +5,23 @@
 //  Created by JeongHwan Seok on 2020/11/12.
 //
 
-import Foundation
+import Swift
 
-print("Hello, World!")
+let constant: String = "차후에 변경 불가능한 상수"
+var variable: String = "차후에 변경 가능한 변수"
 
+variable = "변수 변경"
+
+let sum: Int
+let inputA: Int = 100
+let inputB: Int = 200
+
+sum = inputA + inputB
+print(sum)
+
+var nickName: String
+
+nickName = "bye9"
+print(nickName)
+nickName = "bye1"
+print(nickName)
