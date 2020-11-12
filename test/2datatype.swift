8d380887660e670e3060ee23c9918b4412f5ce42
bye9 <tjrwjdghks12@naver.com>
2020-11-12 20:14:48 +0900

    2data type

---
+++ test/2datatype.swift
@@ -0,0 +1,12 @@
+//
+//  2datatype.swift
+//  test
+//
+//  Created by JeongHwan Seok on 2020/11/12.
+//
+
+import Foundation
+
+var someBool: Bool = true
+someBool = false
+someBool = 0
--- test/main.swift
+++ test/main.swift
@@ -25,3 +25,30 @@ nickName = "bye9"
 print(nickName)
 nickName = "bye1"
 print(nickName)
+//2data type
+
+var someBool: Bool = true
+someBool = false
+
+var someInt: Int = -100
+
+var someUInt: UInt = 100
+
+var someFloat: Float = 3.14
+
+var someDouble: Double = 3.14
+//someDouble = someFloat
+
+var someCharacter: Character = "h"
+
+var someString: String = "하하하"
+someString = someString + "웃어요!"
+print(someString)
+//someString = someCharacter
+
+let integer = 100
+print(type(of: integer))
+let floatingPoint = 12.34
+print(type(of: floatingPoint))
+let apple = "A"
+print(type(of: apple))
