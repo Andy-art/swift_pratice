80ea80f8874a756096771713b78dd705afd3301f
bye9 <tjrwjdghks12@naver.com>
2020-11-12 22:55:09 +0900

    3any,nil

--- test/main.swift
+++ test/main.swift
@@ -52,3 +52,14 @@ let floatingPoint = 12.34
 print(type(of: floatingPoint))
 let apple = "A"
 print(type(of: apple))
+
+//3any,nil
+var someAny: Any = 100
+someAny = "어떤 타입도 수용 가능"
+someAny = 123.12
+
+//let someDouble: Double = someAny
+class SomeClass {}
+var someAnyObject: AnyObject = SomeClass()
+//someAnyObject = 123.12
+//someAny = nil
