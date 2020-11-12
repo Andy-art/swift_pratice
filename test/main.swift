8c96cb4fdb62cd6c35303cca5c33885246157a2e
bye9 <tjrwjdghks12@naver.com>
2020-11-13 00:51:35 +0900

    4array

--- test/main.swift
+++ test/main.swift
@@ -63,3 +63,60 @@ class SomeClass {}
 var someAnyObject: AnyObject = SomeClass()
 //someAnyObject = 123.12
 //someAny = nil
+
+
+//4array
+var integers: Array<Int> = Array<Int>()
+integers.append(1)
+integers.append(100)
+//integers.append(101.1)
+
+print(integers.contains(100))
+print(integers.contains(99))
+
+integers.remove(at: 0)
+integers.removeLast()
+integers.removeAll()
+
+print(integers.count)
+
+var doubles: Array<Double> = [Double]()
+var strings: [String] = [String]()
+var characters: [Character] = []
+
+let immutableArray = [1, 2, 3]
+
+//key, value
+var anyDictionary: Dictionary<String, Any> = [String: Any]()
+anyDictionary["someKey"] = "value"
+anyDictionary["anotherKey"] = 100
+print(anyDictionary)
+anyDictionary["someKey"] = "dictionary"
+
+anyDictionary.removeValue(forKey: "anotherKey")
+anyDictionary["someKey"] = nil
+print(anyDictionary)
+
+let emptyDictionary: [String: String] = [:]
+let initalizedDictionary: [String: String] = ["name": "jeonghwan", "gender": "male"]
+//불확실성 let someValue: String = initalizedDictionary["name"]
+
+var integerSet: Set<Int> = Set<Int>()
+integerSet.insert(1)
+integerSet.insert(100)
+integerSet.insert(99)
+integerSet.insert(99)
+
+print(integerSet.contains(1))
+integerSet.remove(100)
+integerSet.removeFirst()
+print(integerSet.count)
+
+let setA: Set<Int> = [1, 2, 3, 4, 5]
+let setB: Set<Int> = [3, 4, 5, 6, 7]
+
+let union: Set<Int> = setA.union(setB)
+let sortedUnion: [Int] = union.sorted()
+let intersection: Set<Int> = setA.intersection(setB) //교집합
+let subtracting: Set<Int> = setA.subtracting(setB)  //차집합
+
