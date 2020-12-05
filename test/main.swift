5ab34e6815784f7381fa1b72851770973718e7d6
bye9 <tjrwjdghks12@naver.com>
2020-12-05 14:00:53 +0900

    22인스턴스생성소멸

--- test/main.swift
+++ test/main.swift
@@ -887,4 +887,139 @@ Student4.typeMethod()
 Student4.finalClassMethod()
 
 
-//22
+//22인스턴스 생성, 소멸
+//init, deinit
+//스위프트의 모든 인스턴스는 초기화와 동시에 모든 프로퍼티에 유효한 값이 할당되어 있어야 함.
+//프로퍼티에 미리 기본값을 할당해두면 인스턴스가 생성됨과 동시에 초기값을 지니게 됨.
+
+class PersonA {
+    //모든 저장 프로퍼티에 기본값 할당
+    var name: String = "unknown"
+    var age: Int = 0
+    var nickName: String = "nick"
+}
+
+let jason: PersonA = PersonA()
+jason.name = "jason"
+jason.age = 30
+jason.nickName = "j"
+
+//하지만, 인스턴스가 초기화와 동시에 프로퍼티에 원하는 값을 넣어주고 싶음 -> 이니셜라이저 사용
+//프로퍼티 기본값을 사용하지 않고 이니셜라이저를 통해 인스턴스가 가져야 할 초기값을 전달 가능.
+class PersonB {
+    var name: String
+    var age: Int
+    var nickName: String
+    
+    init(name: String, age: Int, nickName: String) {
+        self.name = name
+        self.age = age
+        self.nickName = nickName
+    }
+}
+
+let jerry: PersonB = PersonB(name: "jerry", age: 20, nickName: "제리")
+//let jerry: PersonB = PersonB(name: "jerry", age: 20, nickName: "")
+
+//프로퍼티의 초기값이 꼭 필요 없는 경우가 있을 때
+//옵셔널을 사용
+class PersonC {
+    var name: String
+    var age: Int
+    var nickName: String?
+    
+    convenience init(name: String, age: Int, nickName: String) {
+        //self.name = name
+        //self.age = age
+        //중복된 코드 줄일 수 있음, convenience키워드 필요
+        self.init(name: name, age: age)
+        self.nickName = nickName
+    }
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+let jenny: PersonC = PersonC(name: "jenny", age: 10)
+let mike: PersonC = PersonC(name: "mike", age: 15, nickName: "m")
+
+//암시적 추출 옵셔널 (!느낌표)
+//인스턴스 사용에 꼭 필요하지만 초기값을 할당하지 않고자 할 때 사용
+
+class Puppy {
+    var name: String
+    var owner: PersonC!
+    
+    init(name: String) {
+        self.name = name
+    }
+    func goOut() {
+        print("\(name)가 주인 \(owner.name)와 산책을 합니다")
+    }
+}
+
+let happy: Puppy = Puppy(name: "happy")
+//happy.goOut()
+//주인이 없어서 에러
+happy.owner = jenny
+happy.goOut()
+
+//실패가능한 이니셜라이저
+//이니셜라이저 매개변수로 전달되는 초기값이 잘못된 경우 인스턴스 생성에 실패할 수 있음.
+//그럴 경우 nil을 반환한다.
+class PersonD {
+    var name: String
+    var age: Int
+    var nickName: String?
+    
+    init?(name: String, age: Int) {
+        if (0...120).contains(age) == false {
+            return nil
+        }
+        if name.count == 0 {
+            return nil
+        }
+        self.name = name
+        self.age = age
+    }
+}
+
+let john: PersonD? = PersonD(name: "john", age: 23)
+let joker: PersonD? = PersonD(name: "joker", age: 123)
+let batman: PersonD? = PersonD(name: "", age: 10)
+
+print(joker)    //nil
+print(batman)   //nil
+
+//디이니셜라이저
+//클래스의 인스턴스가 메모리에서 해제되는 시점에 호출되며,
+//그 시점에 해야할 일을 구현할 수 있다.
+class PersonE {
+    var name: String
+    var pet: Puppy?
+    var child: PersonC
+    
+    init(name: String, child: PersonC) {
+        self.name = name
+        self.child = child
+    }
+    
+    deinit {
+        if let petName = pet?.name {
+            print("\(name)가 \(child.name)에게 \(petName)를 인도합니다")
+            self.pet?.owner = child
+        }
+    }
+}
+
+var donald: PersonE? = PersonE(name: "donald", child: jenny)
+donald?.pet = happy
+donald = nil //donald 인스턴스가 더이상 필요없으므로 메모리에서 해제됨.
+//donald가 jenny에게 happy를 인도한다.
+
+
+//23옵셔널체이닝
+
+
